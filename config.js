51ae1ca8ef4b7deda683ee424b013d7db5ff6f20
Chintan Mirani <chintan.mirani@gmail.com>
2023-08-01 19:23:40 +0530

    indentation done

--- config.js
+++ config.js
@@ -1,14 +1,14 @@
 console.log('config');
 module.exports = {
-    db: {
-      database: process.env.DB_NAME || 'Demo',
-      user: process.env.DB_USER || 'root',
-      password: process.env.DB_PASS || 'root',
-      options: {
-        dialect: process.env.DIALECT || 'mysql', // sqlite original
-        host: process.env.HOST || 'localhost',
-        // storage: './intraenvios.sqlite',
-        port: process.env.PORT || 3306 // 8081 original
-      }
+  db: {
+    database: process.env.DB_NAME || 'Demo',
+    user: process.env.DB_USER || 'root',
+    password: process.env.DB_PASS || 'root',
+    options: {
+      dialect: process.env.DIALECT || 'mysql', // sqlite original
+      host: process.env.HOST || 'localhost',
+      // storage: './intraenvios.sqlite',
+      port: process.env.PORT || 3306 // 8081 original
     }
   }
+}
